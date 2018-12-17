f8fb72156d6f9e3b835d6e0296ba1e1a7c177348
enescavas <45938081+enescavas@users.noreply.github.com>
2018-12-17 14:22:03 +0300

    Add files via upload

---
+++ cabukilac.sql
@@ -0,0 +1,83 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 18 Şub 2018, 12:13:39
+-- Sunucu sürümü: 5.7.14
+-- PHP Sürümü: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `cabukilac`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `ayarlar`
+--
+
+CREATE TABLE `ayarlar` (
+  `site_id` int(11) NOT NULL DEFAULT '1',
+  `site_baslik` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `site_url` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `site_tema` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `site_durum` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `ayarlar`
+--
+
+INSERT INTO `ayarlar` (`site_id`, `site_baslik`, `site_url`, `site_tema`, `site_durum`) VALUES
+(1, 'cabukilac', 'http://localhost/Chilty', 'tema1', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `siparisler`
+--
+
+CREATE TABLE `siparisler` (
+  `siparis_id` int(11) NOT NULL,
+  `AdSoyad` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `adres` varchar(300) COLLATE utf8_turkish_ci NOT NULL,
+  `Mtel` varchar(15) COLLATE utf8_turkish_ci NOT NULL,
+  `uyari` int(11) NOT NULL,
+  `ilackodu` varchar(20) COLLATE utf8_turkish_ci NOT NULL,
+  `silinen_siparis` int(11) NOT NULL,
+  `kara_liste` int(11) NOT NULL,
+  `siparis_tarih` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `siparisler`
+--
+ALTER TABLE `siparisler`
+  ADD PRIMARY KEY (`siparis_id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `siparisler`
+--
+ALTER TABLE `siparisler`
+  MODIFY `siparis_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ profil.php
@@ -0,0 +1,14 @@
+<?php 
+
+$kadi = $_SESSION["uye_kadi"];
+
+$bul = $db->prepare("select * from uyeler where uye_kadi=?");
+
+$cek= $bul->execute(array(
+	$kadi
+	));
+$cekk = $cek->fetch();
+
+echo $cekk["eposta"];
+
+?>
